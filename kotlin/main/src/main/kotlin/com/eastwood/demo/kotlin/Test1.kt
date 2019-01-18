9e270ca1738bd972d75c941b18f8725b42ecadfd
yangchengdong <yangchengdong@yingyinglicai.com>
2019-01-18 19:06:43 +0800

    add 'kotlin' library for demo.

---
+++ kotlin/main/src/main/kotlin/com/eastwood/demo/kotlin/Test1.kt
@@ -0,0 +1,15 @@
+package com.eastwood.demo.kotlin
+
+/**
+ *
+ * @author eastwood
+ * createDate: 2019-01-18
+ */
+class Test1 {
+
+    fun test() {
+        // can't use [R.string.kotlin_common] which from microModule ':p_common'.
+        var i = R.string.kotlin_common
+    }
+
+}
---
+++ kotlin/p_common/src/main/java/com/eastwood/demo/kotlin/common/Common.kt
@@ -0,0 +1,14 @@
+package com.eastwood.demo.kotlin.common
+
+/**
+ *
+ * @author eastwood
+ * createDate: 2019-01-18
+ */
+class Common {
+
+    fun test() {
+        var i = R.string.kotlin_common
+    }
+
+}
